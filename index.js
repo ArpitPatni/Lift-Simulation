70abd51ee7ffdc330b8c518591909dce57af87b1
ArpitPatni <arpitpatni00@gmail.com>
2022-10-05 20:08:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const floorsInput=document.querySelector('#floors');
+const liftInput=document.querySelector('#lift');
+const generateFloor=document.querySelector('#btn');
+const building=document.querySelector('.building');
